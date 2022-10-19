1bd89fb41b27cffe0913df459c038a8a9f4d14c6
Aashrit Luthra <aashritluthra@gmail.com>
2022-10-19 13:01:09 -0400

    finish features for top winning countries

--- sql/features/top_winning_countries.sql
+++
@@ -1,7 +0,0 @@
--- Top Winning Countries (1st Place)
-SELECT first_place_country, COUNT(*) FROM WorldCup
-GROUP BY first_place_country
-ORDER BY 2 DESC;
-
--- Top Winning Countries (Podium)
--- TODO
---
+++ sql/features/top_winning_countries_first_place.sql
@@ -0,0 +1,7 @@
+-- Top Winning Countries (First Place)
+CREATE OR REPLACE VIEW FirstPlaceWinners AS
+SELECT first_place_country AS country_name, COUNT(*) AS total_wins FROM WorldCup
+GROUP BY country_name
+ORDER BY total_wins DESC, country_name;
+
+SELECT * FROM FirstPlaceWinners;
