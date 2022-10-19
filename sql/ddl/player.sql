09f9f95aef42745f77ca3fb5266696eb88b88bb3
Anupriyam Ranjit <anupriyamranjit@Anupriyams-MacBook-Air.local>
2022-10-19 16:54:52 -0400

    finished setting up SQL

--- sql/ddl/player.sql
+++ sql/ddl/player.sql
@@ -1,7 +1,10 @@
+-- name: ddl-player-set-foreign-0
 SET FOREIGN_KEY_CHECKS=0;
+-- name: ddl-player-drop-if-exists
 DROP TABLE IF EXISTS Player;
+-- name: ddl-player-set-foreign-1
 SET FOREIGN_KEY_CHECKS=1;
-
+-- name: ddl-player-create-player
 CREATE TABLE IF NOT EXISTS Player (
     name VARCHAR(64),
     shirt_number INT,
--- sql/ddl/world_cup.sql
+++ sql/ddl/world_cup.sql
@@ -1,7 +1,10 @@
+-- name: ddl-world-cup-set-foreign-0
 SET FOREIGN_KEY_CHECKS=0;
+-- name: ddl-world-cup-drop-if-exists
 DROP TABLE IF EXISTS WorldCup;
+-- name: ddl-world-cup-set-foreign-0
 SET FOREIGN_KEY_CHECKS=1;
-
+-- name: ddl-world-cup-create-table
 CREATE TABLE IF NOT EXISTS WorldCup (
     year YEAR PRIMARY KEY,
     host_country VARCHAR(64) NOT NULL,
