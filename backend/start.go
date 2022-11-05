3d5cb96da092e0877be82d278f68f0ff0c4ec33c
Anupriyam Ranjit <anupriyamranjit@Anupriyams-MacBook-Air.local>
2022-11-05 11:56:01 -0400

    Installed Gin

--- backend/start.go
+++ backend/start.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	Greet()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
