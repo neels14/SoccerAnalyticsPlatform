c9176911388c004ed422448ad373d628f9aae047
Aashrit Luthra <aashritluthra@gmail.com>
2022-11-12 22:40:14 -0500

    Populate prod db for four relations

---
+++ sql/prod_data/populate/soccer_match.sql
@@ -0,0 +1,2 @@
+INSERT INTO SoccerMatch
+SELECT DISTINCT Year AS year, Stage AS stage, Home_Team_Name AS home_team_country, Away_Team_Name AS away_team_country, Datetime AS date, City AS city, Stadium AS stadium, Attendance AS attendance FROM WorldCupMatchesCSV;
