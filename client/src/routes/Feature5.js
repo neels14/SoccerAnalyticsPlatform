dc4894b771b80113cbea09727667c97933a39f7a
Hamza Saqib <chaosemerald890@gmail.com>
2022-11-17 14:55:26 -0500

    added front end for features 5 and 6, updated README

--- client/src/routes/Feature5.js
+++ client/src/routes/Feature5.js
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import Paper from '@mui/material/Paper';
+import {StyledTableCell, StyledTableRow} from './shared/StyledTable'
 import { Typography } from "@mui/material";
 
 function Feature5() {
+  const [winRatios, setWinRatios] = useState([])
+
+  useEffect(() => {
+      fetch("/api/v1/country/winRatio")
+          .then(respose => respose.json())
+          .then(json => setWinRatios(json))
+  }, [])
+
   return (
-    <Typography sx={{m: 2}} variant="h5"><b>Feature 5:</b> In Progress</Typography>
+    <>
+      <Typography sx={{m: 2}} variant="h5"><b>Feature 5:</b> Win Ratio</Typography>
+      <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }}>
+              <TableHead>
+                  <StyledTableRow>
+                      <StyledTableCell>Country</StyledTableCell>
+                      <StyledTableCell align="right">Win Ratio</StyledTableCell>
+                  </StyledTableRow>
+              </TableHead>
+              <TableBody>
+                  {winRatios.map((winRatio) => (
+                      <StyledTableRow key={winRatio.country}>
+                          <StyledTableCell>{winRatio.country}</StyledTableCell>
+                          <StyledTableCell align="right">{winRatio.winRatio.toFixed(2)}</StyledTableCell>
+                      </StyledTableRow>
+                  ))}
+              </TableBody>
+          </Table>
+      </TableContainer>
+    </>
   );
 }
 
--- client/src/routes/Feature6.js
+++ client/src/routes/Feature6.js
@@ -1,8 +1,47 @@
+import { useEffect, useState } from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import Paper from '@mui/material/Paper';
+import {StyledTableCell, StyledTableRow} from './shared/StyledTable'
 import { Typography } from "@mui/material";
 
 function Feature6() {
+  const [mostStartedPlayers, setMostStartedPlayers] = useState([])
+
+  useEffect(() => {
+      fetch("/api/v1/players/mostStarted")
+          .then(respose => respose.json())
+          .then(json => setMostStartedPlayers(json))
+  }, [])
+
   return (
-    <Typography sx={{m: 2}} variant="h5"><b>Feature 6:</b> In Progress</Typography>
+    <>
+      <Typography sx={{m: 2}} variant="h5"><b>Feature 6:</b> Most Started Players</Typography>
+      <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }}>
+              <TableHead>
+                  <StyledTableRow>
+                      <StyledTableCell>Country</StyledTableCell>
+                      <StyledTableCell align="right">Player</StyledTableCell>
+                      <StyledTableCell align="right">Shirt Number</StyledTableCell>
+                      <StyledTableCell align="right">Start Ratio</StyledTableCell>
+                  </StyledTableRow>
+              </TableHead>
+              <TableBody>
+                  {mostStartedPlayers.map((mostStartedPlayer) => (
+                      <StyledTableRow key={mostStartedPlayer.country}>
+                          <StyledTableCell>{mostStartedPlayer.country}</StyledTableCell>
+                          <StyledTableCell align="right">{mostStartedPlayer.playerName}</StyledTableCell>
+                          <StyledTableCell align="right">{mostStartedPlayer.shirtNumber}</StyledTableCell>
+                          <StyledTableCell align="right">{mostStartedPlayer.startRatio.toFixed(2)}</StyledTableCell>
+                      </StyledTableRow>
+                  ))}
+              </TableBody>
+          </Table>
+      </TableContainer>
+    </>
   );
 }
 
