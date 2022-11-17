680d81343efdaa1389b8fda40888098bf5eac49c
Hamza Saqib <chaosemerald890@gmail.com>
2022-11-17 16:48:51 -0500

    added country selector to features 5 and 6

--- client/src/routes/Feature5.js
+++ client/src/routes/Feature5.js
@@ -6,19 +6,25 @@ import TableHead from '@mui/material/TableHead';
 import Paper from '@mui/material/Paper';
 import {StyledTableCell, StyledTableRow} from './shared/StyledTable'
 import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import CountrySelect from "./shared/CountrySelect";
 
 function Feature5() {
+  const [country, setCountry] = useState("")
   const [winRatios, setWinRatios] = useState([])
 
   useEffect(() => {
-      fetch("/api/v1/country/winRatio")
+      fetch(`/api/v1/country/winRatio/${country}`)
           .then(respose => respose.json())
-          .then(json => setWinRatios(json))
-  }, [])
+          .then(json => {if (country) {setWinRatios([json])} else {setWinRatios(json)}})
+  }, [country])
 
   return (
     <>
       <Typography sx={{m: 2}} variant="h5"><b>Feature 5:</b> Win Ratio</Typography>
+      <Box margin="20px">
+        <CountrySelect value={country} onChange={event => setCountry(event.target.value)}/>
+      </Box>
       <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }}>
               <TableHead>
