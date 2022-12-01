847d3ecd2d3921c4baa71036be2e3a9aed3eeca5
Hamza Saqib <chaosemerald890@gmail.com>
2022-12-01 18:44:01 -0500

    feature 6 pagination

--- client/src/routes/Feature6.js
+++ client/src/routes/Feature6.js
@@ -8,23 +8,35 @@ import {StyledTableCell, StyledTableRow} from './shared/StyledTable'
 import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import CountrySelect from "./shared/CountrySelect";
+import TablePagination from '@mui/material/TablePagination';
 
 function Feature6() {
   const [country, setCountry] = useState("")
   const [mostStartedPlayers, setMostStartedPlayers] = useState([])
+  const [rowsPerPage, setRowsPerPage] = useState(10)
+  const [count, setCount] = useState()
+  const [page, setPage] = useState(0)
 
   useEffect(() => {
-      fetch(`/api/v1/players/mostStarted/${country}`)
+      fetch(`/api/v1/players/mostStarted/${country}/?limit=${rowsPerPage}&page=${page + 1}`)
           .then(respose => respose.json())
-          .then(json => setMostStartedPlayers(json.data))
-  }, [country])
+          .then(json => {setMostStartedPlayers(json.data); setCount(json.total_count)})
+  }, [country, rowsPerPage, page])
 
   return (
     <>
       <Typography sx={{m: 2}} variant="h5"><b>Feature 6:</b> Most Started Players</Typography>
       <Box margin="20px">
-        <CountrySelect value={country} onChange={event => setCountry(event.target.value)}/>
+        <CountrySelect value={country} onChange={event => {setCountry(event.target.value); setPage(0);}}/>
       </Box>
+      <TablePagination
+          component="div"
+          count={count}
+          page={page}
+          onPageChange={(_event, newPage) => setPage(newPage)}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={event => {setRowsPerPage(parseInt(event.target.value, 10)); setPage(0)}}
+      />
       <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }}>
               <TableHead>
